a4259f563bc37b977ac258d786bba61ecb4dd903
CarLost2312 <Carlos.guti.exe@gmail.com>
2025-07-22 11:45:59 -0300

    anda loco

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('./database'); // conexión a MongoDB
+require('./database');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-// Rutas
 app.use('/api/personaje', require('./routes/personaje.route.js'));
 
-// Puerto SOLO desde Render
 const PORT = process.env.PORT;
-
 app.listen(PORT, () => {
   console.log('✅ Servidor iniciado en el puerto', PORT);
 });
